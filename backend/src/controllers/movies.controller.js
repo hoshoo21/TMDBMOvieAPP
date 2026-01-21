f59b7d9de99f9cd5bd4f8d1155981ccf30d1c979
hoshoo21 <c_gawera@yahoo.com>
2026-01-21 15:00:45 -0800

    movie with list items

--- backend/src/controllers/movies.controller.js
+++ backend/src/controllers/movies.controller.js
@@ -3,6 +3,7 @@ import {searchMovies} from '../services/tmdb.service.js';
 export const search = async(req,res, next)=>{
     try {
          const {query,page} = req.query;
+         console.log(query);
          const movies = await searchMovies(query);
          res.json(movies);
 
--- backend/src/services/tmdb.service.js
+++ backend/src/services/tmdb.service.js
@@ -9,7 +9,6 @@ export const searchMovies =async(query)=>{
     try{
         console.log(query);
         const {data} = await httpClient.get(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=en-US&page=1`);
-        console.log(data);
         return data;
     }
     catch(error){
